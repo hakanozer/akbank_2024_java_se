547039fcff748ec1fa8d40813c7095251e0dce67
Hakan ÖZER <hakanozer02@gmail.com>
2024-10-23 15:13:46 +0300

    Abstract

--- Days-3/src/useabstract/Customer.java
+++ Days-3/src/useabstract/Customer.java
@@ -2,9 +2,14 @@ package src.useabstract;
 
 public class Customer extends Person {
 
+    int accountNumber = 0;
+    public Customer(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
     @Override
     public int accountNumber() {
-        return 100;
+        return accountNumber;
     }
 
 }
--- Days-3/src/useabstract/MainAbstract.java
+++ Days-3/src/useabstract/MainAbstract.java
@@ -3,7 +3,7 @@ package src.useabstract;
 public class MainAbstract {
     public static void main(String[] args) {
 
-        Customer customer = new Customer();
+        Customer customer = new Customer(200);
         System.out.println( customer.accountName() );
         System.out.println( customer.accountTotal() );
 
