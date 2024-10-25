412bc6c6bb89b2fbb187bb2b23c91f3afac1ac7b
Hakan ÖZER <hakanozer02@gmail.com>
2024-10-25 11:09:58 +0300

    File Control

--- Days-5/src/useFile/FileControl.java
+++ Days-5/src/useFile/FileControl.java
@@ -1,7 +1,9 @@
 package src.useFile;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class FileControl {
 
@@ -33,5 +35,28 @@ public class FileControl {
     public void deleteFile() {
         file.delete();
     }
+
+    public void writeToFile(String line) {
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write(line + System.lineSeparator());
+            fileWriter.close();
+        }catch (Exception ex) {
+            System.err.println("Error writing to file: " + ex.getMessage());
+        }
+    }
+
+    public void readFromFile() {
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        }catch (Exception e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+
 
 }
--- Days-5/src/useFile/MainFile.java
+++ Days-5/src/useFile/MainFile.java
@@ -3,8 +3,11 @@ package src.useFile;
 public class MainFile {
     public static void main(String[] args) {
 
-        FileControl fileControl = new FileControl("system");
-        fileControl.createFile();
+        FileControl fileControl = new FileControl();
+        //fileControl.createFile();
         //fileControl.deleteFile();
+        //fileControl.writeToFile("Line-3");
+        fileControl.readFromFile();
+
     }
 }
